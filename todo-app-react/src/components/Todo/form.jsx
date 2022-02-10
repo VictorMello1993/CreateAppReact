22c48f267322ee3f61a99ef0ab26f250fa9a4902
Victor Mello <victorsmello93@gmail.com>
2022-02-10 07:57:40 -0300

    Implementação do Todo list (parte 2)

--- todo-app-react/src/components/Todo/form.jsx
+++ todo-app-react/src/components/Todo/form.jsx
@@ -1,7 +1,29 @@
 import './todo.css'
+import { useState } from 'react'
 
 const Form = () => {
-  return <div className="todoForm">form</div>
+  const [text, setText] = useState('');
+
+  const submitTodo = (event) => {
+    // event.preventDefault();
+    // props.add(text);
+    // return setText('');
+  };
+
+  return (
+    <div className="insert">
+      <input 
+        min={5}
+        type="text" 
+        placeholder="Insira uma tarefa"
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        />
+        <button disabled={!text} onClick={submitTodo}>
+          ADD
+        </button>
+    </div>
+  )
 }
 
 export default Form;

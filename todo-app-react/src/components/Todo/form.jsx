55b2c2bd43d6da9342d56c1d44ef135d9cf831e5
Victor Mello <victorsmello93@gmail.com>
2022-02-10 23:22:21 -0300

    Implementação da adição de um item (task)

--- todo-app-react/src/components/Todo/form.jsx
+++ todo-app-react/src/components/Todo/form.jsx
@@ -1,26 +1,25 @@
 import './todo.css'
 import { useState } from 'react'
 
-const Form = () => {
+const Form = ({add}) => {
   const [text, setText] = useState('');
-
-  const submitTodo = (event) => {
-    // event.preventDefault();
-    // props.add(text);
-    // return setText('');
-  };
+  
+  const submitTodo = (event => {
+    add({task: text})
+    setText('')    
+  });
 
   return (
     <div className="insert">
       <input 
         min={5}
         type="text" 
-        placeholder="Insira uma tarefa"
+        placeholder="Insira uma tarefa..."
         value={text}
         onChange={(event) => setText(event.target.value)}
         />
         <button disabled={!text} onClick={submitTodo}>
-          ADD
+          ADD  
         </button>
     </div>
   )
--- todo-app-react/src/views/Todo.jsx
+++ todo-app-react/src/views/Todo.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import Header from '../components/Header/index'
 import FormTodo from '../components/Todo/form'
+import TodoItem from '../components/Todo/item'
 
 const Todo = () => {
-  const [showForm, setShowForm] = useState(false)
+  const [showForm, setShowForm] = useState(false);
+  const [todo, setTodo] = useState([]);
+
+  const AddTask = (task) => setTodo([...todo, task])
 
   return (
     <div id="todo">
-      <Header handle={setShowForm} value={showForm}/> 
-
-      {/* Renderização condicional em função do estado atribuído no useState()*/}
-      {showForm ? <FormTodo /> : null}           
+      <Header handle={setShowForm} value={showForm}/>       
+      {showForm ? <FormTodo add={AddTask} /> : null}           
+      {todo.map((item, index) => <TodoItem key={index} text={item.task}/>)}      
     </div>
   )
 }

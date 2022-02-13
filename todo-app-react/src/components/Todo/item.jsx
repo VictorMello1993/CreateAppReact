2bb6abb64e1001152bc7519acda5f67f6e0919a0
Victor Mello <victorsmello93@gmail.com>
2022-02-13 11:26:53 -0300

    Revertendo as alterações do item.jsx para o commit onde estava a aula

--- todo-app-react/src/components/Todo/item.jsx
+++ todo-app-react/src/components/Todo/item.jsx
@@ -1,14 +1,10 @@
 import './todo.css'
-import {AiFillCheckCircle} from 'react-icons/ai'
-import {GoTrashcan} from 'react-icons/go'
 
 const TodoItem = ({text}) => {  
   return (
     <div className="item">
       <div className="text">{text}</div>
-      <div className="tooltips">
-        <AiFillCheckCircle size={20}/>
-        <GoTrashcan size={20}/>
+      <div className="tooltips">        
       </div>
     </div>
   )

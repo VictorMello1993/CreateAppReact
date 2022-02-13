023d0a39895d97ef7c5bac812f612362f91969da
Victor Mello <victorsmello93@gmail.com>
2022-02-13 10:57:32 -0300

    Ajustes no css para e index.jsx do header para remover comentários

--- todo-app-react/src/components/Header/index.jsx
+++ todo-app-react/src/components/Header/index.jsx
@@ -2,7 +2,6 @@ import './header.css'
 import {AiFillPlusSquare} from 'react-icons/ai'
 import {AiOutlineMinusSquare} from 'react-icons/ai'
 
-// O handle e value são propriedades do objeto props (parâmetro padrão de uma função de componente)
 const Header = ({handle, value} /*props*/) => {  
 
   // Disparando o evento de click para mostrar o formulário dentro do header

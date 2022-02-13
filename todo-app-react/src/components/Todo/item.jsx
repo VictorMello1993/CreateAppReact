551650856525dbc11903b4f1146c515b63f56d8a
Victor Mello <victorsmello93@gmail.com>
2022-02-13 10:19:04 -0300

    implementada a funcionalidade de marcar como feito

--- todo-app-react/src/components/Todo/item.jsx
+++ todo-app-react/src/components/Todo/item.jsx
@@ -9,22 +9,18 @@ const TodoItem = ({text, item, removeItem, mark}) => {
   const toggleDone = () => {
     mark(item)
 
-    console.log(item)
+    const todoItem = document.querySelector(`div[id=${item.id}]`)    
 
-    const todoItem = document.querySelectorAll('.item')
-
-    console.log(todoItem);
-
-    // if(item.done){
-    //   todoItem.classList.add('done')
-    // }
-    // else {
-    //   todoItem.classList.remove('done')
-    // }    
+    if(item.done){
+      todoItem.classList.add('done')
+    }
+    else {
+      todoItem.classList.remove('done')
+    }    
   }
 
   return (
-    <div className="item">
+    <div className="item" id={item.id}>
       <div className="text">{text}</div>
       <div className="tooltips">
         <AiFillCheckCircle size={20} onClick={toggleDone}/>
